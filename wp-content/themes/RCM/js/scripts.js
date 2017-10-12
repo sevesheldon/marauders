f0c2e0ae104a5a438677fbb4fab10472324c8012
Seve Sheldon <sevesheldon@gmail.com>
2017-10-12 14:24:47 -0700

    Make instructor content CSS more responsive.

--- wp-content/themes/RCM/js/scripts.js
+++ wp-content/themes/RCM/js/scripts.js
@@ -5,6 +5,16 @@
 		'use strict';
 		
 		// DOM ready, take it away
+
+		// var overlay = $('<div id="modal-overlay">This is the overlay</div>');
+		// var popupimg = $('<div id="popupimg"><img src="<?php echo get_template_directory_uri(); ?>/img/bluelogo.png" alt="Logo" ></div>')
+		
+		// $('body').append(overlay, popupimg);
+
+		// $('#modal-overlay').css('opacity', .8).click(function() {
+		//     $(this).fadeOut();
+		// });
+
 		
 	});
 	
